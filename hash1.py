5e73c60c2e352af3f7b3e674c8d65f1c6b5f54c2
Tariqsa1 <56273750+Tariqsa1@users.noreply.github.com>
2019-10-07 20:34:18 +0100

    Delete hash1.py

--- hash1.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-print ("code by Tariq")
-print ("Encryption")
-text = str(input("Enter Text"))
-hash_type = str(input("Enter Hash Type : "))
-text = text.encode ('utf-8')
-hash_hash = hashlib.new(hash_type)
-hasher =hash_hash.hexdigest()
-print (hasher)
